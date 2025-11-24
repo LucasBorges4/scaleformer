8a6c82c08bdf6058368c3406f799c675b1f6eaa4
Lucas <lucasborges-2013@hotmail.com>
2025-11-23 22:12:24 -0300

    xd

--- models/InformerMS.py
+++ models/InformerMS.py
@@ -110,16 +110,19 @@ class Model(nn.Module):
                 dec_out[:, :label_len//scale, :] = self.mv(x_dec[:, :label_len, :], scale)
 
             # cross-scale normalization
-#            mean = torch.cat((enc_out, dec_out[:, label_len//scale:, :]), 1)
- #           mean = mean.mean(1).unsqueeze(1)
-            mean = torch.median(torch.cat((enc_out, dec_out[:, label_len//scale:, :]), 1), dim=1).values.unsqueeze(1)
-          
+            mean = torch.cat((enc_out, dec_out[:, label_len//scale:, :]), 1)
+            mean = mean.mean(1).unsqueeze(1)
+
+            mean = torch.logmean(mean + 1e-5, dim=1).unsqueeze(1)
+
+            
             if self.use_stdev_norm:
                 stdev = torch.sqrt(torch.var(torch.cat((enc_out, dec_out[:, label_len//scale:, :]), 1), dim=1, keepdim=True, unbiased=False)+ 1e-5).detach() 
                 enc_out = enc_out / stdev
                 dec_out = dec_out / stdev
-            enc_out = torch.log(torch.abs(enc_out) + mean + 1e-5)
-            dec_out = torch.log(torch.abs(dec_out) + mean + 1e-5)
+            
+            enc_out = enc_out / mean
+            dec_out = dec_out / mean
 
             enc_out = self.enc_embedding(enc_out, x_mark_enc[:, scale//2::scale], scale=scale, first_scale=scales[0], label_len=label_len)
             enc_out, attns = self.encoder(enc_out)
